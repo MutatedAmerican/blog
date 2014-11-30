e2d7e261d450966ca1f9cd933e8a014d9d23c18c
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-11-30 15:26:59 -0800

    PHP - Blog - 18 Fixing the Path to Our Files

---
+++ model/config.php
@@ -0,0 +1,3 @@
+<?php
+//path to all our project files
+$path= "/diazb-blog/";
--- view/navigation.php
+++ view/navigation.php
@@ -1,6 +1,11 @@
 <!create file to link with other php files>
+<?php
+//require config
+require_once(__DIR__ ."/../model/config.php");
+?>
+
 <nav>
     <url>
-        <li><a href=".../post.php">Blog Post Form</a></li>
+        <li><a href="<?php echo $path. "post.php"?>">Blog Post Form</a></li>
     </url>
 </nav>
