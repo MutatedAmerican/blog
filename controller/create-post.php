aca29fecb62129b949c8f4a2f33bed624785babb
Bryan Diaz <bryandiaz2010@yahoo.com>
2015-01-06 14:44:41 -0800

    PHP - Blog - 23 Establishing a Database Connection

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,5 +1,9 @@
 <!create post and put online database to server!>
     <?php
+    //require database
+    require_once(__DIR__ ."/../model/database.php");
+    //create connection
+    $connection= new mysqli($host, $username, $password, $database);
 //receive info and store into variable
     //filter input from title and make sure its a string
         $title= filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
@@ -9,3 +13,5 @@
         echo "<p>Title: ", $title,"</p>";
     //print out post
         echo "<p>Post: ", $post,"</p>";
+    //close connection
+    $connection->close();
