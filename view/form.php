0675b1e880b1315c51a766bd037d60e3d24ff9f7
Bryan Diaz <bryandiaz2010@yahoo.com>
2015-01-06 14:39:32 -0800

    PHP - Blog - 22 Connecting the Controller to the Form

--- view/form.php
+++ view/form.php
@@ -1,14 +1,21 @@
+<!have access to path variable>
+<?php
+//require config
+require_once(__DIR__ ."/../model/config.php");
+?>
+
 <!file specific for post>
 <h1>Create Blog Post</h1>
 
 <!take in submitted info. and send to table>
-<form>
+<form method="post" action="<?php echo $path. "controller/create-post.php";?>">
     <div>
+    
         <label for="title">Title: </label>
         <input type="text" name="title" />
     </div>
  
-    <div>
+    <div> 
         <label for="post">Post: </label>
         <textarea name="post"></textarea>
     </div>
