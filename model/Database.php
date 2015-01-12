7bd8ba7a363c06d1d7e915f34376d8ed566e5561
Bryan Diaz <bryandiaz2010@yahoo.com>
2015-01-12 15:00:10 -0800

    PHP - Blog - 26 Creating a Database Class

---
+++ model/Database.php
@@ -0,0 +1,11 @@
+<?php
+//creation of class
+class Database{
+    //create instance variables within class
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+}
+
